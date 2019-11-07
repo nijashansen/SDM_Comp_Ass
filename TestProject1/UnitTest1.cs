e7d8cef6f2794a14c56e7d3bac529453c6f8dd92
nijashansen <nijas.w.hansen@gmail.com>
2019-11-07 09:31:14 +0100

    Update UnitTest1.cs

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace TestProject1
         [TestMethod]
         public void TestAvgTru()
         {
-            Assert.IsTrue(Logic.getAverageReview(1) == 4.0oksngoiwngoiwnfoikjwrngj);
+            Assert.IsTrue(Logic.getAverageReview(1) == 4.0);
         }
         
         [TestMethod]
